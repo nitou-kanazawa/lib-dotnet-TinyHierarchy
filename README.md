e31d1e9a6cf7e1b27a12aa015928c11a0d27acd6
Niito <121334451+nitou-kanazawa@users.noreply.github.com>
2025-06-25 21:06:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# template-dotnet-Classlib
+.NET用のテンプレート．Unity環境に合わせて .NET6.0 + C#9.0．
---
+++ tests/MyLib.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace MyLib.Tests {
+
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
